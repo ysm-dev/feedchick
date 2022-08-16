2b54bd106e68447c6ef49eaf7bd690a83d0c64b8
Chris <ysm0622@gmail.com>
2022-08-16 10:24:51 +0800

    init packages

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+export const Index = () => {
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <div>aa</div>
+      <input
+        className="input input-bordered input-sm w-full max-w-xs"
+        type="text"
+        placeholder="RSS Feed URL"
+      />
+      <input
+        className="input input-bordered input-sm w-full max-w-xs"
+        type="text"
+        placeholder="RSS Feed URL"
+      />
+      <input
+        className="input input-bordered input-sm w-full max-w-xs"
+        type="text"
+        placeholder="RSS Feed URL"
+      />
+      <button className="btn btn-primary">Next</button>
+    </div>
+  )
+}
